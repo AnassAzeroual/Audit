353d534373588fb133c07478875a3933d385d9f9
Anass <azeroual.anass2015@gmail.com>
2020-03-17 00:44:57 +0100

    fix design

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,8 @@
-<footer class="container-fluid" id="nav-bottom"><br><br>
+<footer class="container-fluid" id="nav-bottom">
+<div class="flex">
 	<div class="text-primary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur consequatur necessitatibus, maiores.</div>
-<br>
-<br>
-<br>
-<br>
-	<div class="container text-center text-muted"> &#9400;2018@Anass-Azeroual</div><br>
+	<div class="text-muted right"> &#9400;2018@Anass-Azeroual</div>
+</div>
 </footer>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="sources/assets/js/box-info.js"></script>
